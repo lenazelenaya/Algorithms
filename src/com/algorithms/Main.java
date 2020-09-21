4f89fefeaeae79cf38080e8975ec92569fe09405
lenaz <lena.zelenaya@gmail.com>
2020-09-21 08:54:22 +0300

    Add quicksort

--- src/com/algorithms/Main.java
+++ src/com/algorithms/Main.java
@@ -1,8 +1,6 @@
 package com.algorithms;
 
-import com.algorithms.SortingAlgorithms.BubbleSort;
-import com.algorithms.SortingAlgorithms.InsertionSort;
-import com.algorithms.SortingAlgorithms.SelectionSort;
+import com.algorithms.SortingAlgorithms.*;
 
 import java.util.Arrays;
 
@@ -11,12 +9,16 @@ public class Main {
         BubbleSort bubbleSort = new BubbleSort();
         InsertionSort insertionSort = new InsertionSort();
         SelectionSort selectionSort = new SelectionSort();
+        MergeSort mergeSort = new MergeSort();
+        QuickSort quickSort = new QuickSort();
 
         int[] array = {1, 7, 2, 5, 4, 4, 6, 10, 2, 0, 11};
 
         // Arrays.stream(bubbleSort.sort(array)).forEach(System.out::println);
         // Arrays.stream(insertionSort.sort(array)).forEach(System.out::println);
-        Arrays.stream(selectionSort.sort(array)).forEach(System.out::println);
+        // Arrays.stream(selectionSort.sort(array)).forEach(System.out::println);
+        // Arrays.stream(mergeSort.sort(array)).forEach(System.out::println);
+        Arrays.stream(quickSort.sort(array)).forEach(System.out::println);
 
     }
 }
--- src/com/algorithms/SortingAlgorithms/QuickSort.java
+++ src/com/algorithms/SortingAlgorithms/QuickSort.java
@@ -1,5 +1,38 @@
 package com.algorithms.SortingAlgorithms;
 
 public class QuickSort {
+    public int[] sort(int[] array) {
+        quicksort(array, 0, array.length - 1);
+        return array;
+    }
 
+    private void quicksort(int[] array, int left, int right) {
+        if (left < right) {
+            int index = partition(array, left, right);
+
+            quicksort(array, left, index - 1);
+            quicksort(array, index, right);
+        }
+    }
+
+    private int partition(int[] array, int left, int right) {
+        int pivot = array[right];
+        int i = (left - 1);
+
+        for (int j = left; j < right; j++) {
+            if (array[j] <= pivot) {
+                i++;
+                swap(array, i, j);
+            }
+        }
+        swap(array, i + 1, right);
+
+        return i + 1;
+    }
+
+    private void swap(int[] array, int left, int right) {
+        int temp = array[left];
+        array[left] = array[right];
+        array[right] = temp;
+    }
 }
